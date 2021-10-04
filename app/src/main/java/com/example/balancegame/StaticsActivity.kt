4bd88253ceb5724595b59ded4403c4dc98f9488e
goodlucky1215 <goodlucky1215@naver.com>
2021-10-04 16:08:02 +0900

    first commit

---
+++ app/src/main/java/com/example/balancegame/StaticsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.balancegame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StaticsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_statics)
+    }
+}
