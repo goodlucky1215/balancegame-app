2d0eb0d935c4c14f1ef17f87e9c28c3664b2f871
JHoo0118 <kik3078@gmail.com>
2021-11-04 22:33:13 +0900

    fragment_result.xml 작성

--- app/src/main/java/com/example/balancegame/ResultFragment.kt
+++ app/src/main/java/com/example/balancegame/ResultFragment.kt
@@ -1,19 +1,48 @@
 package com.example.balancegame
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.balancegame.databinding.FragmentResultBinding
 
 class ResultFragment : Fragment() {
+
+    private lateinit var resultFragmentBinding: FragmentResultBinding
+
+    var mainActivity: MainActivity? = null
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        resultFragmentBinding = FragmentResultBinding.inflate(layoutInflater, container, false)
+
+        // 메인 페이지로 이동(밸런스 게임 리스트 페이지)
+        resultFragmentBinding.goBalanceGameTextView.setOnClickListener {
+            goMainPage();
+        }
+        resultFragmentBinding.goBalanceGameImageView.setOnClickListener {
+            goMainPage();
+        }
+        //
+
         return inflater.inflate(R.layout.fragment_result, container, false)
+    }
+
+    // 메인 페이지로 이동(밸런스 게임 리스트 페이지)
+    private fun goMainPage() {
+        mainActivity?.let {
+            val intent = Intent(context, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
