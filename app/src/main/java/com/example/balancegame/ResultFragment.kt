de458a65efba226bd5946caac9d17e5b845381c2
JHoo0118 <kik3078@gmail.com>
2021-11-05 23:26:29 +0900

    공유하기 추가

--- app/src/main/java/com/example/balancegame/ResultFragment.kt
+++ app/src/main/java/com/example/balancegame/ResultFragment.kt
@@ -3,6 +3,7 @@ package com.example.balancegame
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,13 +12,8 @@ import com.example.balancegame.databinding.FragmentResultBinding
 
 class ResultFragment : Fragment() {
 
+    private val TAG = "IN_GAME";
     private lateinit var resultFragmentBinding: FragmentResultBinding
-
-    var mainActivity: MainActivity? = null
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        mainActivity = context as MainActivity
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,22 +23,42 @@ class ResultFragment : Fragment() {
 
         // 메인 페이지로 이동(밸런스 게임 리스트 페이지)
         resultFragmentBinding.goBalanceGameTextView.setOnClickListener {
-            goMainPage();
+            Log.i(TAG, "클릭")
+            goMainPage()
         }
         resultFragmentBinding.goBalanceGameImageView.setOnClickListener {
-            goMainPage();
+            Log.i(TAG, "클릭")
+            goMainPage()
         }
-        //
 
-        return inflater.inflate(R.layout.fragment_result, container, false)
+        resultFragmentBinding.shareImageView.setOnClickListener {
+            Log.i(TAG, "공유하기 이미지 클릭")
+            onShareToMyFriend()
+        }
+
+        resultFragmentBinding.shareTextView.setOnClickListener {
+            Log.i(TAG, "공유하기 텍스트 클릭")
+            onShareToMyFriend()
+        }
+
+        return resultFragmentBinding.root
     }
 
     // 메인 페이지로 이동(밸런스 게임 리스트 페이지)
     private fun goMainPage() {
-        mainActivity?.let {
-            val intent = Intent(context, MainActivity::class.java)
-            startActivity(intent)
+        val intent = Intent(context, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun onShareToMyFriend() {
+        val intent = Intent(Intent.ACTION_SEND)
+        with(intent) {
+            addCategory(Intent.CATEGORY_DEFAULT)
+            putExtra(Intent.EXTRA_TEXT, "https://play.google.com/store/apps/details?id=balancegame")
+            putExtra(Intent.EXTRA_TITLE, "밸런스 게임")
+            setType("text/plain")
         }
+        startActivity(Intent.createChooser(intent, "앱을 선택해주세요."))
     }
 
 }
--- app/src/main/java/com/example/balancegame/UrlSave.kt
+++ app/src/main/java/com/example/balancegame/UrlSave.kt
@@ -3,6 +3,7 @@ package com.example.balancegame
 class UrlSave {
     //페이지 ip주소
     val ipNum = "http://10.0.2.2:9090"
+//    val ipNum = "http://8d91-111-118-40-152.ngrok.io"
     //카탈로그 목록 가져오기
     val getCatalogsUrl = "/api/1.0/catalogs"
 }
