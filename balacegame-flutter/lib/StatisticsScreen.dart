ac03e1ce44120ab52590a7c49eef42d3525da4e3
JHoo0118 <kik3078@gmail.com>
2022-01-14 22:44:02 +0900

    통계 수정

--- balacegame-flutter/lib/StatisticsScreen.dart
+++ balacegame-flutter/lib/StatisticsScreen.dart
@@ -52,7 +52,9 @@ class _StatisticsScreenState extends State<StatisticsScreen> {
               builder: (BuildContext context,
                   AsyncSnapshot<dynamic> catalogQuestion) {
                 if (catalogQuestion.hasData == false) {
-                  return CircularProgressIndicator();
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
                 }
                 final catalogQuestionList = catalogQuestion.data.toString();
                 final catalogQuestionLine = catalogQuestionList.split('\n');
@@ -60,62 +62,81 @@ class _StatisticsScreenState extends State<StatisticsScreen> {
                 for (var row in catalogQuestionLine) {
                   catalogQuestionLineSplit.add(row.split('/'));
                 }
-                return SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      StatisticsItemWidget(
-                        catalogQuestionLine[0],
-                        statisticsData.data[0]['result1'],
-                        statisticsData.data[1]['result1'],
-                      ),
-                      StatisticsItemWidget(
-                        catalogQuestionLine[1],
-                        statisticsData.data[0]['result2'],
-                        statisticsData.data[1]['result2'],
-                      ),
-                      StatisticsItemWidget(
-                        catalogQuestionLine[2],
-                        statisticsData.data[0]['result3'],
-                        statisticsData.data[1]['result3'],
-                      ),
-                      StatisticsItemWidget(
-                        catalogQuestionLine[3],
-                        statisticsData.data[0]['result4'],
-                        statisticsData.data[1]['result4'],
-                      ),
-                      StatisticsItemWidget(
-                        catalogQuestionLine[4],
-                        statisticsData.data[0]['result5'],
-                        statisticsData.data[1]['result5'],
-                      ),
-                      StatisticsItemWidget(
-                        catalogQuestionLine[5],
-                        statisticsData.data[0]['result6'],
-                        statisticsData.data[1]['result6'],
-                      ),
-                      StatisticsItemWidget(
-                        catalogQuestionLine[6],
-                        statisticsData.data[0]['result7'],
-                        statisticsData.data[1]['result7'],
-                      ),
-                      StatisticsItemWidget(
-                        catalogQuestionLine[7],
-                        statisticsData.data[0]['result8'],
-                        statisticsData.data[1]['result8'],
-                      ),
-                      StatisticsItemWidget(
-                        catalogQuestionLine[8],
-                        statisticsData.data[0]['result9'],
-                        statisticsData.data[1]['result9'],
-                      ),
-                      StatisticsItemWidget(
-                        catalogQuestionLine[9],
-                        statisticsData.data[0]['result10'],
-                        statisticsData.data[1]['result10'],
-                      ),
-                    ],
-                  ),
-                );
+
+                var aIndex = -1;
+                var bIndex = -1;
+                var currIdx = 0;
+                for (var d in statisticsData.data) {
+                  if (d['catalog']['catalogId'] ==
+                      statisticsData.catalogData.catalogId) {
+                    if (d['choice'] == 1) {
+                      aIndex = currIdx;
+                    } else {
+                      bIndex = currIdx;
+                    }
+                  }
+                  currIdx++;
+                }
+                return aIndex != -1 && bIndex != -1
+                    ? SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            StatisticsItemWidget(
+                              catalogQuestionLine[0],
+                              statisticsData.data[aIndex]['result1'],
+                              statisticsData.data[bIndex]['result1'],
+                            ),
+                            StatisticsItemWidget(
+                              catalogQuestionLine[1],
+                              statisticsData.data[aIndex]['result2'],
+                              statisticsData.data[bIndex]['result2'],
+                            ),
+                            StatisticsItemWidget(
+                              catalogQuestionLine[2],
+                              statisticsData.data[aIndex]['result3'],
+                              statisticsData.data[bIndex]['result3'],
+                            ),
+                            StatisticsItemWidget(
+                              catalogQuestionLine[3],
+                              statisticsData.data[aIndex]['result4'],
+                              statisticsData.data[bIndex]['result4'],
+                            ),
+                            StatisticsItemWidget(
+                              catalogQuestionLine[4],
+                              statisticsData.data[aIndex]['result5'],
+                              statisticsData.data[bIndex]['result5'],
+                            ),
+                            StatisticsItemWidget(
+                              catalogQuestionLine[5],
+                              statisticsData.data[aIndex]['result6'],
+                              statisticsData.data[bIndex]['result6'],
+                            ),
+                            StatisticsItemWidget(
+                              catalogQuestionLine[6],
+                              statisticsData.data[aIndex]['result7'],
+                              statisticsData.data[bIndex]['result7'],
+                            ),
+                            StatisticsItemWidget(
+                              catalogQuestionLine[7],
+                              statisticsData.data[aIndex]['result8'],
+                              statisticsData.data[bIndex]['result8'],
+                            ),
+                            StatisticsItemWidget(
+                              catalogQuestionLine[8],
+                              statisticsData.data[aIndex]['result9'],
+                              statisticsData.data[bIndex]['result9'],
+                            ),
+                            StatisticsItemWidget(
+                              catalogQuestionLine[9],
+                              statisticsData.data[aIndex]['result10'],
+                              statisticsData.data[bIndex]['result10'],
+                            ),
+                          ],
+                        ),
+                      )
+                    : const Center(
+                        child: Text("통계를 가져오지 못했습니다."),
+                      );
               },
             ),
     );
